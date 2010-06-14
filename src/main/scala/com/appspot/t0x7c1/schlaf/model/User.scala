d385ae1c38794cd60ec73847feedb361f53f54ad
jmtaro <jmtaro@gmail.com>
2010-06-15 01:05:44 +0900

    リファクタ
     * 無駄な抽象化を排除
     * User は case class 化
     ** copy メソッドの利用で fragment が不要になった
     * model の実装を別なオブジェクトに委譲した
     ** 継承排除 & i/f の公開範囲を厳密に決定できるようになった

--- src/main/scala/com/appspot/t0x7c1/schlaf/model/User.scala
+++ src/main/scala/com/appspot/t0x7c1/schlaf/model/User.scala
@@ -4,51 +4,37 @@ import com.google.appengine.api.{datastore => ds}
 import com.appspot.t0x7c1.zelt.core.gae
 import com.appspot.t0x7c1.zelt.{model => md}
 
-class UserNotFoundException(val target: String) extends Exception
 
-class User (
+case class User (
   val id: String,
   val nickname: String,
-  val hashkey: String = UserModel.createHashkey ) extends md.UniqueItem
-{
-  def update(fragment: UserFragment) = UserModel.update(this, fragment)
+  val hashkey: String = UserData.createHashkey ) extends md.UniqueItem
+
+object UserModel {
+  def find = UserData.find _
+  def put = UserData.put _
+  def count = UserData.count
 }
 
-class UserFragment (
-  var nickname: Option[String] = None ) extends md.UniqueItemFragment
-
-object UserModel extends md.UniqueItemModel with gae.Logger{
-  override type Item = User
-  override type Fragment = UserFragment
+private[model] object UserData extends md.UniqueItemData with gae.Logger{
+  type Item = User
 
   def kind = "user"
 
-  def createFragment = new UserFragment
+  def rebuild(item: Item) = item.copy(hashkey = createHashkey)
 
-  def update(item: Item, fragment: Fragment): Item =
-    new User(
-      id = item.id,
-      nickname = fragment.nickname getOrElse item.nickname,
-      hashkey = fragment.hashkey getOrElse item.hashkey
-    )
+  def itemToEntity(item: Item) = toEntity(item){ -- =>
+    --set "id" -> item.id
+    --set "nickname" -> item.nickname
+    --set "hashkey" -> item.hashkey
+  }
 
-  def entityToItem(entity: ds.Entity): Item = {
-    val -- = new PropertyInspector(entity)
+  def entityToItem(entity: ds.Entity) = toItem(entity){ -- =>
     new User(
       id = --asString "id",
       nickname = --asString "nickname",
-      hashkey = --asString "hashkey"
-    )
-  }
-
-  def itemToEntity(item: Item): ds.Entity = {
-    val key = ds.KeyFactory.createKey(UserModel.kind, item.id)
-    val entity = new ds.Entity(key)
-    val prop = new PropertyInspector(entity)
-    prop set "id" -> item.id
-    prop set "nickname" -> item.nickname
-    prop set "hashkey" -> item.hashkey
-    entity
+      hashkey = --asString "hashkey" )
   }
 
 }
+
--- src/main/scala/com/appspot/t0x7c1/zelt/model/src.scala
+++ src/main/scala/com/appspot/t0x7c1/zelt/model/src.scala
@@ -4,123 +4,112 @@ import com.google.appengine.api.{datastore => ds}
 import java.util.ConcurrentModificationException
 import java.util.UUID
 
-abstract class BasicItem
+import com.appspot.t0x7c1.zelt.model
 
-abstract class BasicItemFragment
 
-abstract class BasicItemModel{
-  type Item <: BasicItem
-  type Fragment <: BasicItemFragment
+abstract class Item
 
-  def kind: String
-  def createFragment: Fragment
-  def update(item: Item, fragment: Fragment): Item
-
-  def itemToEntity(item: Item): ds.Entity
-  def entityToItem(entity: ds.Entity): Item
-
-  def count: Int = {
-    val query = new ds.Query(kind)
-    getService.prepare(query).countEntities
-  }
-
-  protected def getService = ds.DatastoreServiceFactory.getDatastoreService
-
-  protected class PropertyInspector(entity: ds.Entity){
-    def as[A] = entity.getProperty(_: String).asInstanceOf[A]
-    def asString = as[String]
-    def asDate = as[java.util.Date]
-    def set [A](kv: (String, A)) = entity.setProperty(kv._1, kv._2)
-  }
-
-  protected class StartTransaction{
-    lazy val service = ds.DatastoreServiceFactory.getDatastoreService
-    lazy val transaction = service.beginTransaction
-  }
-
-  protected trait InTransaction {
-    val service: ds.DatastoreService
-    val transaction: ds.Transaction
-  }
-
-  protected trait BasicItemSetter extends InTransaction{
-    def put (item: Item) {
-      try{
-        val entity = itemToEntity(item)
-        service.put(transaction, entity)
-        transaction.commit
-      } catch {
-        case e: ConcurrentModificationException =>
-          if (transaction.isActive) transaction.rollback
-          throw e
-      }
-    }
-  }
-
-  protected trait BasicItemGetter extends InTransaction{
-    def notFound (item: Item): Boolean =
-      try{
-        service.get(transaction, itemToEntity(item).getKey)
-        transaction.rollback
-        false
-      } catch {
-        case e: ds.EntityNotFoundException => true
-      }
-  }
-
-}
-
-abstract class UniqueItem extends BasicItem{
+abstract class UniqueItem extends Item{
+  def id: String
   def hashkey: String
 }
 
-abstract class UniqueItemFragment extends BasicItemFragment{
-  var hashkey: Option[String] = None
+private class TransactionService(
+  service: ds.DatastoreService, transaction: ds.Transaction )
+{
+  def get (key: ds.Key) = { service.get(transaction, key) }
+  def put (entity: ds.Entity) { service.put(transaction, entity) }
 }
 
-abstract class UniqueItemModel extends BasicItemModel{
-  type Item <: UniqueItem
-  type Fragment <: UniqueItemFragment
+private object Operation {
+  def getService = ds.DatastoreServiceFactory.getDatastoreService
 
-  def put(item: Item) {
-    val op = new StartTransaction with UniqueItemSetter with UniqueItemGetter
-    op confirmNotModified item
-    op put item
+  def count (query: ds.Query): Int = getService.prepare(query).countEntities
+
+  def transact (block: TransactionService => Unit){
+    val service = getService
+    val transaction = service.beginTransaction
+    try{
+      block(new TransactionService(service, transaction))
+      transaction.commit
+    } catch {
+      case e: Exception =>
+        if (transaction.isActive) transaction.rollback
+        throw e
+    }
   }
+
+}
+
+trait ItemConverter {
+  type Item <: model.Item
+
+  def entityToItem(entity: ds.Entity): Item
+  def itemToEntity(item: Item): ds.Entity
+  def itemToKey(item: Item): ds.Key
+
+  implicit def convertFromItem(item: Item) = new {
+    def toEntity = itemToEntity(item)
+    def toKey = itemToKey(item)
+  }
+
+  implicit def convertFromEntity(entity: ds.Entity) = new {
+    def toItem = entityToItem(entity)
+  }
+
+}
+
+class PropertyInspector(entity: ds.Entity){
+  def as [A] = entity.getProperty(_: String).asInstanceOf[A]
+  def asString = as[String]
+  def asDate = as[java.util.Date]
+  def set [A](kv: (String, A)) = entity.setProperty(kv._1, kv._2)
+}
+
+trait UniqueItemData extends ItemConverter{
+  type Item <: model.UniqueItem
+
+  def rebuild(item: Item): Item
+
+  def kind: String
 
   def createHashkey = UUID.randomUUID.toString
 
+  def itemToKey(item: Item): ds.Key = ds.KeyFactory.createKey(kind, item.id)
+
+  def count = Operation count new ds.Query(kind)
+
+  def put (item: Item) = Operation.transact{ service =>
+    try{
+      val current = service.get(item.toKey).toItem
+      if (current.hashkey != item.hashkey)
+        throw new ConcurrentModificationException
+    } catch {
+      case e: ds.EntityNotFoundException =>// noop
+    }
+    service put rebuild(item).toEntity
+  }
+
   def find (id: String): Option[Item] = {
-    val query = new ds.Query(kind)
-    query.addFilter("id", ds.Query.FilterOperator.EQUAL, id)
-
-    getService.prepare(query).asSingleEntity match {
-      case null => None
-      case entity => Some(entityToItem(entity))
+    val service = Operation.getService
+    try {
+      val key = ds.KeyFactory.createKey(kind, id)
+      Option(service.get(key).toItem)
+    } catch {
+      case e: ds.EntityNotFoundException => None
     }
   }
 
-  protected trait UniqueItemGetter extends InTransaction{
-    def confirmNotModified (item: Item) {
-      try{
-        val entity = service.get(transaction, itemToEntity(item).getKey)
-        val current = entityToItem(entity)
-        if (current.hashkey != item.hashkey){
-          transaction.rollback
-          throw new ConcurrentModificationException
-        }
-      } catch {
-        case e: ds.EntityNotFoundException =>// noop
-      }
-    }
+  protected def toEntity (item: Item)(block: PropertyInspector => Unit): ds.Entity = {
+    val entity = new ds.Entity(item.toKey)
+    val inspector = new PropertyInspector(entity)
+    block(inspector)
+    entity
   }
 
-  protected trait UniqueItemSetter extends BasicItemSetter{
-    override def put (item: Item) {
-      val fragment = createFragment
-      fragment.hashkey = Some(createHashkey)
-      super.put(update(item, fragment))
-    }
+  protected def toItem [A <: Item](entity: ds.Entity)(block: PropertyInspector => A): A = {
+    val inspector = new PropertyInspector(entity)
+    block(inspector)
   }
 
 }
